4921cb1f3067016086e52ebf44e8ec57570d7376
Pablo <pablo.garcia2@live.u-tad.com>
2022-11-08 11:24:49 +0100

    Modificacion 3

--- src/PGSM.java
+++ src/PGSM.java
@@ -4,7 +4,8 @@ public class PGSM {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alumno; Pablo_Garcia");
-		System.out.println("Primera Modificacion desde Master");
+		System.out.println("Primera Modificacion desde Master - Cambiado");
+		
 	}
 
 }
